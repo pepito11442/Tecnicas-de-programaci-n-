5e45c87f168f0435058a90e0e560fdee1ba3e6bb
Paolo Valdivia <paolovaldivia2008@gmail.com>
2025-05-30 19:37:04 -0500

    Grupo-D (Final).py

--- Grupo-D (Final).py
+++ Grupo-D (Final).py
@@ -3,30 +3,36 @@ import Libreria.salir as salir
 import Libreria.submenu as sub
 
 def menu():
-    print("\n=========== MENÚ DE OPCIONES ===========\n")
-    print("1. Procesar jugadores")
-    print("2. Mostrar estadísticas")
-    print("3. Salir")
-
     while True:
-        try:
-            op = int(input("Ingrese opción del menú: "))
-            if op in [1, 2, 3]:
-                return op
+        print("\n=========== MENÚ DE OPCIONES ===========\n")
+        print("1. Procesar jugadores")
+        print("2. Mostrar estadísticas")
+        print("3. Salir")
+
+        while True:
+            op = input("Ingrese opción del menú: ")
+
+            if op == "1" or op == "2" or op == "3":
+                return int(op)
             else:
-                print("ERROR. Opción inválida.")
-        except ValueError:
-            print("ERROR. Ingrese un número.")
+                print("ERROR. Opción inválida. Ingrese 1, 2 o 3.")
+                print("\n=========== MENÚ DE OPCIONES ===========\n")
+                print("1. Procesar jugadores")
+                print("2. Mostrar estadísticas")
+                print("3. Salir")
 
 def ejecutar():
     while True:
         opcion = menu()
-        if opcion == 1:
-            proc.procesar()
-        elif opcion == 2:
-            sub.submenu_estadisticas()
-        elif opcion == 3:
-            salir.salir()
-            break
+        match opcion:
+            case 1:
+                proc.procesar()
+            case 2:
+                sub.submenu_estadisticas()
+            case 3:
+                salir.salir()
+            case _:
+                print("Error")
 
 ejecutar()
+
